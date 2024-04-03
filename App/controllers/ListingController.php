951328c9f7d580fd192ef8ab8e5c4a098ea49d4a
2022Dong <faithhopelove777111@gmail.com>
2024-04-03 22:21:16 +0800

    Form Submission & Sanitizing Data

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -68,4 +68,18 @@ class ListingController
             'listing' => $listing
         ]);
     }
+    public function store()
+    {
+        $allowedFields = ['title', 'description', 'salary', 'requirements', 'benefits', 'company', 'address', 'city', 'state', 'phone', 'email'];
+
+        // Turn value into key. --array_flip()
+        $newListingData = array_intersect_key($_POST, array_flip($allowedFields));
+
+        $newListingData['user_id'] = 1;
+
+        $newListingData = array_map('sanitize', $newListingData);
+
+        //inspectAndDie($_POST);
+        inspectAndDie($newListingData);
+    }
 }
