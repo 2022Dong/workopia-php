4c842b53cbe954b000ba5803c4eb9fa429d0dd1e
2022Dong <faithhopelove777111@gmail.com>
2024-04-07 11:35:30 +0800

    add .gitignore files/folders

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dong/workopia-php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8a655ece9a0c76d9744db5f98f4ce957c5636039',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dong/workopia-php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8a655ece9a0c76d9744db5f98f4ce957c5636039',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
